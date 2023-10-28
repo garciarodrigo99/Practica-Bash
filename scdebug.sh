472b7da46fbee1d61143a3f8d1c2dad30d7daea0
Rodrigo <alu0101154473@ull.edu.es>
2023-10-28 22:48:19 +0100

    Funcion n_attach

--- scdebug.sh
+++ scdebug.sh
@@ -40,147 +40,10 @@ n_attach_vector=()
 p_attach_vector=()
 prog=()
 
-#------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 ### PROGRAMA
 
-### Funciones 
-error_exit()
-{
-
-        lastValue="${!#}"   # Guarda el último argumento pasado por parámetro
-        echo "${PROGNAME}: Error: ${1:-"Error desconocido"}" 1>&2
-        usage
-        echo 
-        echo "Saliendo con ${lastValue}"
-        exit $lastValue
-}
-
-is_option()
-{
-    #echo "Llamada is_option con argumento "$1""
-    for elemento in "${PROGRAM_OPTIONS[@]}"; do
-        if [ "$elemento" == "$1" ]; then
-            return 0  # Retorna 0 para indicar éxito (cadena encontrada)
-        fi
-    done
-
-    return 1  # Retorna 1 para indicar que la cadena no fue encontrada
-
-}
-
-consult ()
-{
-
-    for ((i = 1; i < ${#v_option[@]}; i++)); do
-        # Si es la primera posicion del vector es vacia salto a la siguiente iteración
-        if [ -z "${v_option[i]}" ]; then
-            continue
-        fi
-        # Si el directorio no existe salto a la siguiente iteración
-        folder="${HOME}/.scdebug/${v_option[i]}/"
-        if [[ ! -e "$folder" ]]; then
-            continue
-        fi
-        # Nombre de comanda
-        command="${v_option[i]}"
-        # (Solo) programas ordenados mod más reciente
-        output="ls -lt --time-style=long-iso "$folder""
-
-        # Volcar las filas de la salida por pantalla al vector rows
-        rows=()
-        while IFS= read -r linea; do
-            rows+=("$linea")
-            # Verificar si option es igual a -v y salir después de la primera línea
-            if [ "${v_option[0]}" == "-v"  ]; then
-                break
-            fi
-        done < <($output)
-
-        # Eliminar la primera posición del vector
-        rows=("${rows[@]:1}")
-
-        # Recorrer vector el vector de filas
-        for linea in "${rows[@]}"; do
-            #echo $linea
-            local_file_name=$(echo "$linea" | cut -d' ' -f8)
-            file_time=$(echo "$linea" | cut -d' ' -f6,7)
-            echo "=============== COMMAND: "$command" ======================="
-            echo "=============== TRACE FILE: "$local_file_name" ================="
-            echo "=============== TIME: "$file_time" =============="
-            echo
-        done
-
-    done
-}
-
-# Cambia el nombre de los programas por su proceso más reciente
-fill_n_attach() 
-{
-    if [ "${n_attach_vector[0]}" == "" ]; then
-        return 1
-    fi
-
-    for ((i = 1; i < ${#n_attach_vector[@]}; i++)); do
-        nombre_programa="${n_attach_vector[i]}"
-        pid=$(pgrep -u ${USER} -n ${nombre_programa})    # -u: usuario 
-                                                    # -n: FLAG + reciente
-
-        if [ -n "$pid" ]; then
-            n_attach_vector[i]=$pid
-        else
-            n_attach_vector[i]=""       # Valor predeterminado si el programa no está en ejecución
-        fi
-    done
-
-    # No es necesario devolver el vector actualizado ya que n_attach_vector es una variable global
-    echo "Procesos N más recientes: ${n_attach_vector[*]}"
-}
-
-# Cambia el numero de proceso por el nombre de su comando
-fill_p_attch()
-{
-    if [ "${p_attach_vector[0]}" == "" ]; then
-        return 1
-    fi
-
-    for ((i = 1; i < ${#p_attach_vector[@]}; i++)); do
-        numero_programa="${p_attach_vector[i]}"
-        # ps -p 14574 -o comm=
-        pid=$(ps -p ${numero_programa} -o comm=)
-
-        if [ -n "$pid" ]; then
-            p_attach_vector[i]=$pid
-        else
-            p_attach_vector[i]=""       # Valor predeterminado si el programa no está en ejecución
-        fi
-    done
-
-    # No es necesario devolver el vector actualizado ya que n_attach_vector es una variable global
-    echo "Procesos P más recientes: ${p_attach_vector[*]}"
-}
-
-createFolders()
-{
-    # Crear directorio .scdebug
-    if [[ ! -e "${HOME}/.scdebug" ]]; then
-    mkdir "${HOME}/.scdebug"
-    fi
-
-    # Crear directorio con el nombre del programa
-    if [ -n "${prog[0]}" ]; then
-        if [[ ! -e "${HOME}/.scdebug/${prog[0]}" ]]; then
-            mkdir ${HOME}/.scdebug/${prog[0]}
-        fi
-    fi
-
-    # Crear directorio con el nombre del programa -nattch
-    if [ -n "${n_attach_vector[0]}" ]; then
-        if [[ ! -e "${HOME}/.scdebug/${n_attach_vector[0]}" ]]; then
-            mkdir ${HOME}/.scdebug/${n_attach_vector[0]}
-        fi
-    fi
-}
-
+#### Funciones genéricas
 usage()
 {
 	echo
@@ -217,6 +80,186 @@ _EOF_
 
 }
 
+error_exit()
+{
+
+        lastValue="${!#}"   # Guarda el último argumento pasado por parámetro
+        echo "${PROGNAME}: Error: ${1:-"Error desconocido"}" 1>&2
+        usage
+        echo 
+        echo "Saliendo con ${lastValue}"
+        exit $lastValue
+}
+
+is_option()
+{
+    #echo "Llamada is_option con argumento "$1""
+    for elemento in "${PROGRAM_OPTIONS[@]}"; do
+        if [ "$elemento" == "$1" ]; then
+            return 0  # Retorna 0 para indicar éxito (cadena encontrada)
+        fi
+    done
+
+    return 1  # Retorna 1 para indicar que la cadena no fue encontrada
+
+}
+
+#### Funciones para modularizar el programa
+
+# Funcion para ejecutar la opción -v(all)
+consult ()
+{
+
+    for ((i = 1; i < ${#v_option[@]}; i++)); do
+        # Si es la primera posicion del vector es vacia salto a la siguiente iteración
+        if [ -z "${v_option[i]}" ]; then
+            continue
+        fi
+        # Si el directorio no existe salto a la siguiente iteración
+        folder="${HOME}/.scdebug/${v_option[i]}/"
+        if [[ ! -e "$folder" ]]; then
+            continue
+        fi
+        # Nombre de comanda
+        command="${v_option[i]}"
+        # (Solo) programas ordenados mod más reciente
+        output="ls -lt --time-style=long-iso "$folder""
+
+        # Volcar las filas de la salida por pantalla al vector rows
+        rows=()
+        while IFS= read -r linea; do
+            rows+=("$linea")
+            # Verificar si option es igual a -v y salir después de la primera línea
+            if [ "${v_option[0]}" == "-v"  ]; then
+                break
+            fi
+        done < <($output)
+
+        # Eliminar la primera posición del vector
+        rows=("${rows[@]:1}")
+
+        # Recorrer vector el vector de filas
+        for linea in "${rows[@]}"; do
+            local_file_name=$(echo "$linea" | cut -d' ' -f8)
+            file_time=$(echo "$linea" | cut -d' ' -f6,7)
+            echo "=============== COMMAND: "$command" ======================="
+            echo "=============== TRACE FILE: "$local_file_name" ================="
+            echo "=============== TIME: "$file_time" =============="
+            echo
+        done
+
+    done
+
+    exit 0
+}
+
+# Cambia el nombre de los programas por su proceso más reciente
+fill_n_attach() 
+{
+    for ((i = 1; i < ${#n_attach_vector[@]}; i++)); do
+        nombre_programa="${n_attach_vector[i]}"
+        pid=$(pgrep -u ${USER} -n ${nombre_programa})    # -u: usuario 
+                                                    # -n: FLAG + reciente
+
+        if [ -n "$pid" ]; then
+            n_attach_vector[i]=$pid
+        else
+            n_attach_vector[i]=""       # Valor vacio si el programa no está en ejecución
+        fi
+    done
+
+    # No es necesario devolver el vector actualizado ya que n_attach_vector es una variable global
+    echo "Procesos N más recientes: ${n_attach_vector[*]}"
+}
+
+# Cambia el numero de proceso por el nombre de su comando
+fill_p_attch()
+{
+
+    for ((i = 1; i < ${#p_attach_vector[@]}; i++)); do
+        numero_programa="${p_attach_vector[i]}"
+        # ps -p 14574 -o comm=
+        pid=$(ps -p ${numero_programa} -o comm=)
+
+        if [ -n "$pid" ]; then
+            p_attach_vector[i]=$pid
+        else
+            p_attach_vector[i]=""       # Valor predeterminado si el programa no está en ejecución
+        fi
+    done
+
+    # No es necesario devolver el vector actualizado ya que n_attach_vector es una variable global
+    echo "Procesos P más recientes: ${p_attach_vector[*]}"
+}
+
+createFolders()
+{
+    # Crear directorio .scdebug
+    if [[ ! -e "${HOME}/.scdebug" ]]; then
+    mkdir "${HOME}/.scdebug"
+    fi
+
+    # Crear directorio con el nombre del programa
+    if [ -n "${prog[0]}" ]; then
+        if [[ ! -e "${HOME}/.scdebug/${prog[0]}" ]]; then
+            mkdir ${HOME}/.scdebug/${prog[0]}
+        fi
+    fi
+
+    # Crear directorio con el nombre del programa -pattch
+    if [ -n "${p_attach_vector[0]}" ]; then
+        if [[ ! -e "${HOME}/.scdebug/${p_attach_vector[0]}" ]]; then
+            mkdir ${HOME}/.scdebug/${p_attach_vector[0]}
+        fi
+    fi
+}
+
+n_attach_function()
+{
+    echo "Funcion n_attach_function"
+    # Si no está activa la opción salir de la funcion
+    if [ "${n_attach_vector[0]}" == "" ]; then
+        return 1
+    fi
+    echo ${n_attach_vector[@]}
+    # Cambiar los nombres de programa por su proceso más reciente
+    for ((i = 1; i < ${#n_attach_vector[@]}; i++)); do
+        program_name="${n_attach_vector[i]}"
+        pid=$(pgrep -u ${USER} -n ${program_name})   # -u: usuario 
+                                                        # -n: FLAG + reciente
+
+        # Crear directorio con el nombre del programa -nattch
+        if [ -n "${n_attach_vector[i]}" ]; then
+            if [[ ! -e "${HOME}/.scdebug/${n_attach_vector[i]}" ]]; then
+                mkdir ${HOME}/.scdebug/${n_attach_vector[i]}
+            fi
+        fi
+
+        echo "$pid"
+        if [ -n "$pid" ]; then
+            n_attach_vector[i]=$pid
+            local_file_name="trace_$(uuidgen).txt"
+            route=${HOME}/.scdebug/$program_name/${local_file_name}
+            sudo strace -p $pid -o $route &
+        # else
+        #     n_attach_vector[i]=""       # Valor vacio si el programa no está en ejecución
+        fi
+    done
+
+}
+
+p_attach_function()
+{
+    echo "Funcion p_attach_function"
+    if [ "${p_attach_vector[0]}" == "" ]; then
+        return 1
+    fi
+    fill_p_attch
+
+}
+
+# -----------------------------------------------------------------------------
+
 ### Main
 
 # While para leer primero todas las opciones
@@ -247,7 +290,6 @@ while [ "$1" != "" ]; do
                 shift
             done
             consult
-            exit 0
             # No lanzar operaciones strace
             ;;
         -nattch )
@@ -304,18 +346,14 @@ done
 
 # # Varios if para no anidar bucles
 
-fill_n_attach
-fill_p_attch
-
 createFolders
-
+n_attach_function
+exit 0
 filename="trace_$(uuidgen).txt"
 route=${HOME}/.scdebug/${prog[0]}/${filename}
 
 aux="${sto_option} ${n_attach_vector[@]} -o ${route} ${prog[@]} : ejecutado"
 echo $aux
-# sto_option y attachVector pueden estar vacios ya que almacena un simbolo vacio
-# y a la hora de pasarselo a un comando lo tomará como un espacio.
 strace ${sto_option} ${n_attach_vector[@]} -o ${route} ${prog[@]}
 
 # Preguntar(¿?)
